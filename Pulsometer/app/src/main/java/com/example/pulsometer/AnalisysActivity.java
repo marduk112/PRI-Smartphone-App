c0c3c4993d2e07d187a33f573336c61f0bc02bf4
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-06-17 11:28:04 +0200

    Puls is displayed on pie

--- Pulsometer/app/src/main/java/com/example/pulsometer/AnalisysActivity.java
+++ Pulsometer/app/src/main/java/com/example/pulsometer/AnalisysActivity.java
@@ -1,5 +1,7 @@
 package com.example.pulsometer;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -24,32 +26,52 @@ public class AnalisysActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
         setContentView(R.layout.activity_analisys);
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6),
+        graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+
         });
+        graph.getViewport().setMinY(50);
+        graph.getViewport().setMaxY(100);
+        /*graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(100);*/
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setScrollable(true);
+        graph.getViewport().setScalable(true);
+
+        graph.addSeries(series);
         GlobalVariables.Pulses.addListEventListener(new ChangeListEventListener());
     }
     public class ChangeListEventListener implements ListEventListener<Integer> {
         @Override
         public void listChanged(ListEvent<Integer> listChanges) {
             EventList<Integer> tempList = listChanges.getSourceList();
-            for (Integer temp : tempList) {
+            for (final Integer temp : tempList) {
                 if (temp > 0) {
                     System.out.println("Event pulses " + temp);
-                    TextView t = (TextView)findViewById(R.id.textView2);
-                    t.append("Pulse: " + temp + "\n");
+                    ((Activity)context).runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //TextView t = (TextView)findViewById(R.id.textView2);
+                            //t.append("Pulse: " + temp + "\n");
+                            series.appendData(new DataPoint(x, temp), true, 10000);
+                            x += 0.25;
+                            graph.getViewport().computeScroll();
+                        }
+                    });
+                    //
+                    //
                 //    listChanges.next();
                 }
             }
         }
     }
-    private static int x = 0;
+    private LineGraphSeries<DataPoint> series;
+    private GraphView graph;
+    private Context context;
+    private static double x = 0;
     /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
