81c677681e2b6dcd1a3980cc96f32833ab9abb65
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-12-18 13:41:26 +0100

    add method for check if event is passed

--- Pulsometer/app/src/main/java/com/example/pulsometer/PedometerActivity.java
+++ Pulsometer/app/src/main/java/com/example/pulsometer/PedometerActivity.java
@@ -6,7 +6,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.activeandroid.query.Select;
+import com.example.pulsometer.Model.EventViewModel;
+import com.example.pulsometer.Model.PedometerSqlite;
 import com.example.pulsometer.R;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.EventListener;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class PedometerActivity extends Activity {
 
@@ -16,4 +29,35 @@ public class PedometerActivity extends Activity {
         setContentView(R.layout.activity_pedometer);
     }
 
+    private void checkEvents() throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        List<EventViewModel> eventViewModelList = new Select()
+                .from(EventViewModel.class)
+                .execute();
+        for (EventViewModel eventViewModel : eventViewModelList) {
+            Set<Date> dates = new HashSet<>();
+            Date startDate = dateFormat.parse(eventViewModel.StartDateTimeEvent);
+            Date endDate = dateFormat.parse(eventViewModel.EndDateTimeEvent);
+            dates.add(startDate);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(startDate);
+            do {
+                calendar.add(Calendar.DAY_OF_MONTH, 1);
+                dates.add(calendar.getTime());
+            } while(!calendar.getTime().equals(endDate));
+            long stepsNumber = 0;
+            for (Date date : dates) {
+                PedometerSqlite pedometerSqlite = new Select()
+                        .from(PedometerSqlite.class)
+                        .where("Date = ?", date)
+                        .executeSingle();
+                stepsNumber += pedometerSqlite.StepsNumber;
+            }
+            if (stepsNumber >= eventViewModel.StepsNumber) {
+                System.out.println("Event passed!!!");
+            } else {
+                System.out.println("Event not passed!!!");
+            }
+        }
+    }
 }
